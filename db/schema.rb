d2e62d7ab7860502e38913cbb25b19f46ae5f55e
Drew Keat <thedrewkeat@gmail.com>
2021-06-24 14:27:58 -0600

    done

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210624194327) do
+ActiveRecord::Schema.define(version: 20210624200618) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "balance", precision: 11, scale: 2, default: "0.0"
   end
 
 end
